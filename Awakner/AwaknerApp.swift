5ba5fc89245cca239052f842de71f24d1b30cc87
Personal <personal@neocustic-mac-mini-m1.home>
2023-03-01 18:15:23 +0100

    first commit

---
+++ Awakner/AwaknerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwaknerApp.swift
+//  Awakner
+//
+//  Created by Personal on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AwaknerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
